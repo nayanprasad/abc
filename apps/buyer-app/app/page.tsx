d4615673abb77f788d0dbf89451e1a92e4d0981b
nayanprasad <nayanprasad096@gmail.com>
2024-12-02 00:12:15 +0530

    prettier init

--- apps/buyer-app/app/page.tsx
+++ apps/buyer-app/app/page.tsx
@@ -1,11 +1,6 @@
 import { AxiosAPI } from "api";
 
-
 export default function Home() {
-    // AxiosAPI.getInstance().billboardService.createBillboard()
-    return (
-        <div className={"bg-red-50 text-red-800 text-2xl"} >
-            buyer app
-        </div>
-    );
+  // AxiosAPI.getInstance().billboardService.createBillboard()
+  return <div className={"bg-red-50 text-red-800 text-2xl"}>buyer app</div>;
 }
--- packages/api/index.tsx
+++ packages/api/index.tsx
@@ -1,59 +1,74 @@
-import type {AxiosInstance} from "axios";
+import type { AxiosInstance } from "axios";
 import axios from "axios";
-import {BillboardControllerApi, OrganizationControllerApi, UserControllerApi,} from "./src/axios";
-
+import {
+  BillboardControllerApi,
+  OrganizationControllerApi,
+  UserControllerApi,
+} from "./src/axios";
 
 export class AxiosAPI {
-    private static frontEndInstance: AxiosAPI;
-    private static frontEndAxiosInstance: AxiosInstance;
+  private static frontEndInstance: AxiosAPI;
+  private static frontEndAxiosInstance: AxiosInstance;
 
-    private static backendInstance: AxiosAPI;
-    private static backendEndAxiosInstance: AxiosInstance;
+  private static backendInstance: AxiosAPI;
+  private static backendEndAxiosInstance: AxiosInstance;
 
-    private static baseApiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+  private static baseApiUrl =
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
 
-    public static getFrontEndAxiosInstance(): AxiosInstance {
-        return this.frontEndAxiosInstance
+  public static getFrontEndAxiosInstance(): AxiosInstance {
+    return this.frontEndAxiosInstance;
+  }
+
+  public static getInstance(): AxiosAPI {
+    if (!AxiosAPI.frontEndInstance) {
+      AxiosAPI.frontEndAxiosInstance = axios.create({
+        baseURL: process.env.NEXT_PUBLIC_API_URL,
+        headers: {
+          "X-Organization-ID": localStorage.getItem("organizationId"),
+        },
+      });
+
+      AxiosAPI.frontEndAxiosInstance.interceptors.request.use(
+        async (request) => {
+          //todo: validate token here
+          return request;
+        },
+      );
+      AxiosAPI.frontEndInstance = new AxiosAPI();
     }
+    return AxiosAPI.frontEndInstance;
+  }
 
-    public static getInstance(): AxiosAPI {
-        if (!AxiosAPI.frontEndInstance) {
-            AxiosAPI.frontEndAxiosInstance = axios.create({
-                baseURL: process.env.NEXT_PUBLIC_API_URL,
-                headers: {
-                    "X-Organization-ID": localStorage.getItem("organizationId"),
-                },
-            });
-
-            AxiosAPI.frontEndAxiosInstance.interceptors.request.use(
-                async (request) => {
-                    //todo: validate token here
-                    return request;
-                },
-            );
-            AxiosAPI.frontEndInstance = new AxiosAPI();
-        }
-        return AxiosAPI.frontEndInstance;
+  public static getBackendInstance(): AxiosAPI {
+    if (!AxiosAPI.backendInstance) {
+      AxiosAPI.backendEndAxiosInstance = axios.create({
+        baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
+      });
+      AxiosAPI.backendInstance = new AxiosAPI();
     }
+    return AxiosAPI.backendInstance;
+  }
 
-    public static getBackendInstance(): AxiosAPI {
-        if (!AxiosAPI.backendInstance) {
-            AxiosAPI.backendEndAxiosInstance = axios.create({
-                baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
-            });
-            AxiosAPI.backendInstance = new AxiosAPI();
-        }
-        return AxiosAPI.backendInstance;
-    }
+  public userService: UserControllerApi;
+  public organizationService: OrganizationControllerApi;
+  public billboardService: BillboardControllerApi;
 
-    public userService: UserControllerApi;
-    public organizationService: OrganizationControllerApi;
-    public billboardService: BillboardControllerApi;
-
-    constructor() {
-        this.userService = new UserControllerApi(undefined, AxiosAPI.baseApiUrl, AxiosAPI.frontEndAxiosInstance);
-        this.organizationService = new OrganizationControllerApi(undefined, AxiosAPI.baseApiUrl, AxiosAPI.frontEndAxiosInstance);
-        this.billboardService = new BillboardControllerApi(undefined, AxiosAPI.baseApiUrl, AxiosAPI.frontEndAxiosInstance);
-    }
-
+  constructor() {
+    this.userService = new UserControllerApi(
+      undefined,
+      AxiosAPI.baseApiUrl,
+      AxiosAPI.frontEndAxiosInstance,
+    );
+    this.organizationService = new OrganizationControllerApi(
+      undefined,
+      AxiosAPI.baseApiUrl,
+      AxiosAPI.frontEndAxiosInstance,
+    );
+    this.billboardService = new BillboardControllerApi(
+      undefined,
+      AxiosAPI.baseApiUrl,
+      AxiosAPI.frontEndAxiosInstance,
+    );
+  }
 }
